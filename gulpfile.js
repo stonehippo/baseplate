a7356d68fd4b1493f9942ce614c642900499a863
Paul Demers <paul@demersdesigns.com[mergetool Kaleidoscope]>
2015-08-24 22:49:03 -0400

    syntax error

--- gulpfile.js
+++ gulpfile.js
@@ -68,9 +68,7 @@ gulp.task('styleguide:generate', function() {
 //Process CSS
 gulp.task('sass', function() {
     return gulp.src(sassSource)
-            outputStyle: 'expanded',
-            errLogToConsole: true
-        }))
+        .pipe(sass({ outputStyle: 'expanded', errLogToConsole: true }))
         .pipe(autoprefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest(rootPath + 'css'))
         //.pipe(gulp.dest(rootPath + 'styleguide/public'))
